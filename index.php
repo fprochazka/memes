bbf6b29269af66e7ba6539f706967a21a7df19c0
Filip Procházka <hosiplan@gmail.com>
2012-03-26 08:38:34 +0200

    Face names

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ function e($s, $attr = FALSE) {
 function is_image($file) {
 	return !is_dir($file) && in_array(strtolower(pathinfo($file, PATHINFO_EXTENSION)), array('jpg', 'jpeg', 'png', 'gif'));
 }
+function name($file) {
+	echo trim(preg_replace_callback('/(^|\\-)+(.)/', function ($match) { return ' '.strtoupper($match[2]); }, pathinfo($file, PATHINFO_FILENAME)));
+}
 
 ?><!DOCTYPE html>
 <html>
@@ -16,16 +19,22 @@ function is_image($file) {
 		.container { margin-left: auto; margin-right: auto; *zoom: 1; width: 940px; }
 		.container:before, .container:after { display: table; content: ""; }
 		.container:after { clear: both; }
-		.container a { display:block; float:left; width: 130px; height: 100px; margin: 10px 10px 0 0; text-align:center; }
+		.container .face { display:block; float:left; width: 130px; height: 130px; margin: 10px 10px 0 0; text-align:center; }
+		.container a { display:block; width: 130px; height: 100px; }
 		.container a img { max-height:100%; max-width:100%; }
 	</style>
 	<title>Rage faces</title>
 </head>
 <body>
 
-	<div class="container"><?php foreach (glob(__DIR__ . '/faces/*') as $file): if(!is_image($file)) continue; ?>
-		<a href="faces/<?php e(basename($file)) ?>"><img src="faces/<?php e(basename($file)) ?>" alt="<?php e(basename($file)) ?>"></a>
-	<?php endforeach; ?>
+	<div class="container"><?php foreach (glob(__DIR__ . '/faces/*') as $file): if(!is_image($file)) continue ?>
+		<div class="face">
+			<a href="faces/<?php e(basename($file)) ?>">
+				<img src="faces/<?php e(basename($file)) ?>" alt="<?php e(basename($file)) ?>">
+			</a>
+			<?php name($file) ?>
+		</div>
+	<?php endforeach ?>
 	</div>
 
 </body>
